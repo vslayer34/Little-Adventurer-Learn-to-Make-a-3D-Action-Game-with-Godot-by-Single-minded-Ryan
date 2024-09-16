909da6e1fa029712720db0ad3f805d20bc34acdf
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-09-16 18:00:41 +0300

    minor tweeks

--- Scripts/PlayerCharacter.cs
+++ Scripts/PlayerCharacter.cs
@@ -3,8 +3,15 @@ using System;
 
 public partial class PlayerCharacter : CharacterBody3D
 {
+	[ExportGroup("Required Nodes")]
+	[Export]
+	public Node3D Visual { get; private set; }
+
+
 	public const float SPEED = 5.0f;
 	public const float JUMP_VELOCITY = 4.5f;
+
+	private Vector2 _lookDirection;
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -24,6 +31,7 @@ public partial class PlayerCharacter : CharacterBody3D
 			);
 		
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		
 		if (direction != Vector3.Zero)
 		{
 			velocity.X = direction.X * SPEED;
@@ -33,6 +41,23 @@ public partial class PlayerCharacter : CharacterBody3D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, SPEED);
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, SPEED);
+		}
+
+		if (velocity.Length() > 0.2f)
+		{
+			_lookDirection.X = velocity.Z;
+			_lookDirection.Y = velocity.X;
+
+			// Visual.Rotation = new Vector3(Visual.Rotation.X, _lookDirection.Angle(), Visual.Rotation.Z);
+			// Visual.RotateY()
+			Visual.RotateObjectLocal(Vector3.Up, RotationDegrees.X);
+			
+			var a = new Quaternion(Transform.Basis);
+			var b = new Quaternion(Visual.Transform.Basis);
+
+			var c = a.Slerp(b,0.5f);
+
+			// Visual.Transform = new Transform3D(c);
 		}
 
 		Velocity = velocity;
