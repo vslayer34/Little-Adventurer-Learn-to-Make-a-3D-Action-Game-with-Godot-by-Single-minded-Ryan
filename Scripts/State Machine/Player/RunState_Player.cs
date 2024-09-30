d57a609ed4211f31abbb45f913b6fb755bb94fee
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-09-30 14:02:49 +0300

    Update RunState_Player.cs

--- Scripts/State Machine/Player/RunState_Player.cs
+++ Scripts/State Machine/Player/RunState_Player.cs
@@ -1,11 +1,22 @@
 using System;
 using Godot;
+using LittleAdventurer.Scripts.Helper;
 
 namespace LittleAdventurer.Scripts.State_Machine.Player;
-public partial class RunState_Player : StateBase
+public partial class RunState_Player : State_Player
 {
     protected override void EnterState(StateBase state)
     {
         base.EnterState(state);
+        Player.AnimPlayer.Play(AnimationConsts.Player.RUN);
+    }
+
+    protected override void UpdateState(float delta)
+    {
+        if (Player.MoveDirection == Vector3.Zero)
+        {
+            _stateMachine.SwitchStates<IdleState_Player>();
+            GD.Print("Switched to idle");
+        }
     }
 }
